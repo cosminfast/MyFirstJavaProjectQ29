f12d3318fd150a092db622a475d83d3625e0b135
sebastianciocan <seby.ciocan@gmail.com>
2022-11-21 18:15:04 +0200

    control flow statements and animal rescue

---
+++ src/Language.java
@@ -0,0 +1,10 @@
+public class Language {
+    String name="Lala";
+
+    Language() {
+        System.out.println("This is the default constructor");
+    }
+    Language(String text){
+        name = text;
+    }
+}
